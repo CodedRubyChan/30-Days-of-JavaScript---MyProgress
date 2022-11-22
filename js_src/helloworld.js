fe8ed82799213bc58aaa15b67227bf41eaf925ae
CodedRubyChan <emrex1210@gmail.com>
2022-11-22 22:56:29 +0100

    Beispiele getestet :D
    Ich habe hier Beispiele von Inline Scripts, Internal Scripts & External Scripts erlernt :D

---
+++ js_src/helloworld.js
@@ -0,0 +1,1 @@
+console.log('Dies ist die "helloworld.js" Datei die als Externes Script abgerufen wird :D')
---
+++ js_src/introduction.js
@@ -0,0 +1,2 @@
+console.log('Dies ist die "introduction.js" Datei die als Externes Script abgerufen wird :D')
+console.log('Fun Fact über Ruby: Sie ist 19 Jahre alt und liebt Kingdom Hearts! :D')
